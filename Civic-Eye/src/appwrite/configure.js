824f9836d6299142ee0e8d6764b6c4ee191b79ef
Shivang <kshivang679@gmail.com>
2025-10-11 21:49:11 +0530

    fix: correct spacing in UpdateIssue method and update upVotes property in IssueCard component

--- Civic-Eye/src/appwrite/configure.js
+++ Civic-Eye/src/appwrite/configure.js
@@ -110,7 +110,7 @@ export class Services {
 
   async UpdateIssue(
     IssueId,
-    { title, description, status, location, severity,upVotes}
+    { title, description, status, location, severity, upVotes }
   ) {
     try {
       return await this.databases.updateDocument(
@@ -123,7 +123,7 @@ export class Services {
           status,
           location,
           severity,
-          upVotes
+          upVotes,
         }
       );
     } catch (error) {
